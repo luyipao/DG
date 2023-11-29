e2b1dd955dfa773c2a20a25181936bc3213927b1
luyipao <luyipao6@gmail.com>
2023-11-30 00:11:35 +0800

    Euler法包装实现

--- #1/Untitled.m
+++ #1/Untitled.m
@@ -1,74 +1,108 @@
-%%
-f = @(t) cos(t);
-% base function psizei
-base_func = @(t,xi,j,size) ((t-xi).^j / (size.^j)) .* (j>=1) + 1.0 .* (j ==0) ;
-% derivation of base function psizei
-diff_base_func = @(t,xi,j,size) (j >=1) .* (j .* (t-xi).^(j-1) / size^j) ;
-
-
-%% 
-u = @(t) sin(t);
-a = 0;
-b = 2;
-initial_value = 0;
-for p = [1,inf]
-    for degree = [0,1,2]
-        for size = [0.1, 1/20, 1/40, 1/80,1/160, 1/ 320]
-            u_h = DG(f,  a,b,initial_value, size,degree,base_func,diff_base_func);
-            e_h = error_vec(u,u_h,a,b);
-            disp(['h is ', num2str(size), '; k is: ', num2str(degree), '; in integral [', num2str(a), ', ', num2str(b), ' ], norm is : ', num2str(p), ', e_h is ', num2str(norm(e_h,p))]);
+f = @(x) 2 * pi * cos(2* pi .*x);
+% u(x,t) = sin(x - t)
+%input: 区间[xa,xb]；cell宽度size；解空间多项式最高阶degree；t=0时函数u(x,0)=u_0
+% output: 系数矩阵C，其列向量c_i表示在cell I_i上的u的系数
+u_0 =@(t)  sin(2*pi .* t);
+u = @(x,t) sin(2*pi .*(x-t));
+for h = [1/10,1/20,1/40]
+     degree = 1;
+    [u_hh,U_h] = RKDG(f,0,1,0,1,u_0,h,degree);
+    x = 0:h:1;
+    t = 0:h:1;
+    U = zeros(1/h+1,1/h+1);
+    for ii = 1:1/h+1
+        for tt = 1:1/h+1
+            U(tt,ii) = u(x(ii),t(tt));
         end
+    end
+    E_h = U - U_h;
+    disp(['h is ', num2str(h), '; k is: ', num2str(degree), '; error is ', num2str(norm(E_h,1) / (1/(h^2)))]);
+end
+
+
+function [u_hh,D] = RKDG(f,xa,xb,ta,tb,u_0,h,degree)
+    X = xa:h:xb;
+    h_t = (tb-ta) / length(X);
+    [u_h,C] = DG(f,xa,xb,u_0(xa),h,degree);
+    [K,N] = size(C);
+     u_hh = cell(length(X),1);% t_n 上近似[xa,xb]的u_h
+    A = zeros(degree + 1, degree+1);
+    for tt = 1:N
+        u_hh{tt} = builder(C,X);
+        [~,C] = Euler_solve(h_t, X, C);
+    end
+    u_hh{length(X)} = builder(C,X);
+    D = zeros(length(X),length(X));
+    for tt = 1:length(X)
+        D(tt,:) = u_hh{tt}(X);
     end
 end
 
-%% Function
-% input: size间隔；degree基函数的阶；区间[a,b]；f；initial_value初值;base_func
-% 基函数；diff_base_func基函数的导数
-% output: u_h数值解
-% tdl: 该DG于base绑定，只是适用当前base function
-function u_hh = DG(f,lEndpoint,rEndpoint,initial_value,size,degree,base_func,diff_base_func) 
-N = fix((rEndpoint-lEndpoint)/size);
-A = zeros(degree+1,degree+1);
-b = zeros(degree+1,1);
-u_h = cell(1, N);
-x = lEndpoint:size:rEndpoint;
-u_hh = @(t) 0;
+function [u_hh, C] = Euler_solve(t, X, C)
+        u_h = builder(C,X);
+        [K,N] = size(C);
+        A = zeros(K,K);
+        for ii = 1:N
+            for m = 1:K
+                for jj = 1:K
+                    [phi_m,~] = base_func(m-1,X(ii),X(ii+1));
+                    [phi_jj,~] = base_func(jj-1,X(ii),X(ii+1));
+                    A(m,jj) = integral(@(x) phi_m(x) .* phi_jj(x), X(ii), X(ii+1));
+                end
+            end
+            b = L(u_h, X(ii), X(ii+1), K);   
+            C(:,ii) = C(:,ii) + t  * (A\b);
+        end
+        u_hh = builder(C,X);
+end
+
+
+%L base on base_function, 
+% input: t_n时初值u^{(n)}，时间间隔t，算子L(x)
+% ouput: u^{(n+1)}
+
+function u = RK3(u,t,L,xa,xb,K)
+u1 = u + t * L(u,xa,xb,K);
+u2 = 3 / 4 * u + 1/4 * (u1 + t .* L(u1));
+u = 1/3 * u + 2/3 * (u2 + t .* L(u2));
+end
+
+
+% input: 区间[a,b]，阶k
+% output: phi [a,b]上基函数; diff_pfhi phi的导函数
+function u_hh = builder(C,X)
+[K, N] = size(C);
+u_h = cell(1,N);
 for i = 1:N
     u_h{i} = @(t) 0;
 end
-
+u_hh = @(t) 0;
 for ii = 1:N
-for m = 1:degree+1
-    for j = 1:degree+1
-        A(m,j) = -1.0 .* integral(@(t) base_func(t,x(ii),j-1,size).*diff_base_func(t,x(ii), m-1, size),  x(ii),  x(ii+1)) + base_func(x(ii+1),x(ii),j-1,size).*base_func(x(ii+1),x(ii),m-1,size);
+    for jj = 1:K
+        [phi,~] = base_func(jj-1,X(ii),X(ii+1));
+        u_h{ii} = @(t) u_h{ii}(t) + C(jj,ii) * phi(t);
     end
-    b(m) = initial_value .* base_func(x(ii),x(ii),m-1,size) + integral(@(t) base_func(t,x(ii),m-1,size).*f(t),x(ii),x(ii+1));
+    u_hh  = @(t) u_hh(t) + (X(ii) < t) .*(t <= X(ii+1)) .* u_h{ii}(t);
 end
-c = A \ b;
-for ll = 1:(degree+1)
-    u_h{ii} = @(t) u_h{ii}(t) +  c(ll) .* base_func(t,x(ii),ll-1,size);
+u_hh = @(t) u_hh(t) + (t==X(1)) .* u_h{1}(t);
 end
-if (ii ~= N)
-    initial_value = u_h{ii}(x(ii+1));
+
+
+% input: 阶k, cell [a,b]
+% output: 基函数phi和其导数diff_phi
+function [phi, diff_phi] = base_func(k,xa,xb)
+h = xb - xa;
+phi = @(x) ((x-xa).^k / (h^k)) * (k>=1) + 1.0 * (k ==0);
+% derivation of base function phi
+diff_phi = @(x) (k >=1) * (k * (x-xa).^(k-1) / h^k);
+end
+
+% u函数；xa cell左端点；xb cell右端点；K 基函数的维数
+%output: b RK法中的f(t,y)
+function b = L(u,xa,xb,K)
+b = zeros(K,1);
+for ii = 1:K
+    [phi,diff_phi] = base_func(ii,xa,xb);
+    b(ii) = integral(@(x) u(x) .* diff_phi(x), xa, xb) - u(xb) * phi(xb) + u(xa) * phi(xa);
 end
 end
-for jj = 1:N
-    u_hh = @(t) u_hh(t) + ( x(jj) <= t) .*(t < x(jj+1)) .* u_h{jj}(t);
-end
-u_hh = @(t) u_hh(t) + (t == rEndpoint) .* u_h{N}(t);
-end
-%figure
-% input: u真实函数；u_h数值解函数，[a,b]网格区间
-% output: 真实函数图像和数值解函数图像
- function figure(u,u_h,a,b)
-    x = a:0.001:b;
-    plot(x,u(x),'g',x,u_h(x),'--');
-    legend('exact solution','numericial solution');
- end
- % e_h
- % input: u真实函数；u_h数值解函数，[a,b]网格区间
- % output: e_h误差向量
- function e_h = error_vec(u,u_h,a,b)
-    x = a:0.001:b;
-    e_h = u(x) - u_h(x);
- end
--- #1/homework2.m
+++ #1/homework2.m
@@ -5,7 +5,7 @@ f = @(x) 2 * pi * cos(2* pi .*x);
 u_0 =@(t)  sin(2*pi .* t);
 u = @(x,t) sin(2*pi .*(x-t));
 for h = [1/10,1/20,1/40]
-     degree = 2;
+     degree = 1;
     [u_hh,U_h] = RKDG(f,0,1,0,1,u_0,h,degree);
     x = 0:h:1;
     t = 0:h:1;
@@ -16,30 +16,22 @@ for h = [1/10,1/20,1/40]
         end
     end
     E_h = U - U_h;
-    disp(['h is ', num2str(h), '; k is: ', num2str(degree), '; error is ', num2str(norm(E_h,1))]);
+    disp(['h is ', num2str(h), '; k is: ', num2str(degree), '; error is ', num2str(norm(E_h,1) / (1/(h^2)))]);
 end
-function [u_hh,D] = RKDG(f,xa,xb,ta,tb,u_0,size,degree)
-    X = xa:size:xb;
+
+% output 时间t_1到t_n的函数u_hh；网格上的数值解矩阵D
+function [u_hh,D] = RKDG(f,xa,xb,ta,tb,u_0,h,degree)
+    X = xa:h:xb;
     N = length(X) - 1;
     h_t = (tb-ta) / length(X);
-    A = zeros(degree + 1, degree+1);
     u_hh = cell(length(X),1);% t_n 上近似[xa,xb]的u_h
-    [u_h,C] = DG(f,xa,xb,u_0(xa),size,degree);
+    [~,C] = DG(f,xa,xb,u_0(xa),h,degree);
     for tt = 1:N
-        u_hh{tt} = builder(C,xa,xb,size);
-        for ii = 1:N
-            for m = 1:degree+1
-                for jj = 1:degree+1  
-                    [phi_m,~] = base_func(m,X(ii),X(ii+1));
-                    [phi_jj,~] = base_func(jj,X(ii),X(ii+1));
-                    A(m,jj) = integral(@(x) phi_m(x) .* phi_jj(x), X(ii), X(ii+1));
-                end
-            b = L(u_h, X(ii), X(ii+1), degree + 1);    
-            end
-            C(:,ii) = C(:,ii) + h_t  * (A\b);
-        end
+        u_hh{tt} = builder(C,X);
+        [~,C] = Euler_solve(u_hh{tt}, h_t, X, C);
     end
-    u_hh{length(X)} = builder(C,xa,xb,size);
+    u_hh{N+1} = builder(C,X);
+    
     D = zeros(length(X),length(X));
     for tt = 1:length(X)
         D(tt,:) = u_hh{tt}(X);
@@ -47,28 +39,72 @@ function [u_hh,D] = RKDG(f,xa,xb,ta,tb,u_0,size,degree)
 end
 
 
+%input: 区间[xa,xb], 基函数维数+1  K；算子L；时间间隔t；函数u；步长 h
+%output:  欧拉迭代后的系数矩阵C；阶段函数 u_hh；
+function [u_hh, C] = Euler_solve(u, delta_t, X, C)
+[K, N] = size(C);
+A = zeros(K,K);
+for ii = 1:N
+    for m = 1:K
+        for jj = 1:K
+            [phi_m,~] = base_func(m-1,X(ii),X(ii+1));
+            [phi_jj,~] = base_func(jj-1,X(ii),X(ii+1));
+            A(m,jj) = integral(@(x) phi_m(x) .* phi_jj(x), X(ii), X(ii+1));
+        end
+    end
+    b = L(u, X(ii), X(ii+1), K);
+    C(:,ii) = C(:,ii) + delta_t  * (A\b);
+end
+u_hh = builder(C,X);
+end
+
+% 
+function [u_hh,C] = RK2(u_h,delta_t,X,C)
+[u1,C1] = Euler_solve(u_h, delta_t,X,C);
+[u2, C2] = Euler_solve(u1, delta_t,X,C);
+C = 0.5 * (C + C2);
+u_hh = @(t) 0.5 * u_h(t) + 0.5 * u2(t);
+end
+
 %L base on base_function, 
 % input: t_n时初值u^{(n)}，时间间隔t，算子L(x)
 % ouput: u^{(n+1)}
+% function u = RK2(u,t,L,xa,xb,K)
+% u1 = u + t * L(u,xa,xb,K);
+% u2 = 3 / 4 * u + 1/4 * (u1 + t .* L(u1));
+% u = 1/3 * u + 2/3 * (u2 + t .* L(u2));
+% end
 
-function u = RK3(u,t,L,xa,xb,K)
-u1 = u + t * L(u,xa,xb,K);
-u2 = 3 / 4 * u + 1/4 * (u1 + t .* L(u1));
-u = 1/3 * u + 2/3 * (u2 + t .* L(u2));
-end
 
+% input: 系数C，区间[xa,xb], 步长 h
+% output: 近似函数 u_hh
+% function u_hh = builder(C,xa,xb,h)
+% X = xa:h:xb;
+% N = length(X) - 1;
+% u_h = cell(1, N);
+% for i = 1:N
+%     u_h{i} = @(t) 0;
+% end
+% u_hh = @(t) 0;
+% [K, ~] = size(C);
+% for ii = 1:N
+%     for jj = 1:K
+%         [phi,~] = base_func(jj-1,X(ii),X(ii+1));
+%         u_h{ii} = @(t) u_h{ii}(t) + C(jj,ii) * phi(t);
+%     end
+%     u_hh  = @(t) u_hh(t) + (X(ii) < t) .*(t <= X(ii+1)) .* u_h{ii}(t);
+% end
+% u_hh = @(t) u_hh(t) + (t == xa) .* u_h{1}(t);
+% end
 
-% input: 区间[a,b]，阶k
-% output: phi [a,b]上基函数; diff_phi phi的导函数
-function u_hh = builder(C,xa,xb,h)
-X = xa:h:xb;
-N = length(X) - 1;
-u_h = cell(1, N);
+%函数u_hh
+function u_hh = builder(C,X)
+[K, N] = size(C);
+u_h = cell(1,N);
 for i = 1:N
     u_h{i} = @(t) 0;
 end
 u_hh = @(t) 0;
-[K, ~] = size(C);
 for ii = 1:N
     for jj = 1:K
         [phi,~] = base_func(jj-1,X(ii),X(ii+1));
@@ -76,10 +112,11 @@ for ii = 1:N
     end
     u_hh  = @(t) u_hh(t) + (X(ii) < t) .*(t <= X(ii+1)) .* u_h{ii}(t);
 end
-u_hh = @(t) u_hh(t) + (t == xa) .* u_h{1}(t);
+u_hh = @(t) u_hh(t) + (t==X(1)) .* u_h{1}(t);
 end
 
-% input: 阶k, cell [a,b]
+
+% input: 多项式的阶k, cell [a,b]
 % output: 基函数phi和其导数diff_phi
 function [phi, diff_phi] = base_func(k,xa,xb)
 h = xb - xa;
